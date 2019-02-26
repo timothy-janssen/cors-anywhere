d56e91cf73dc068e13fc3ec83e08e45cca8a2e9e
Timothy Janssen <timothy.janssen@sap.com>
2019-02-26 09:27:24 -0800

    Update server.js

--- server.js
+++ server.js
@@ -40,5 +40,5 @@ cors_proxy.createServer({
     xfwd: false,
   },
 }).listen(port, host, function() {
-  console.log('Running CORS Anywhere on ' + host + ':' + port);
+  console.log('Running CORS Anywhere on ' + host + ':' + port); 
 });
